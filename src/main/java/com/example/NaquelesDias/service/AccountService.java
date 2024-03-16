84c69b4f8bf247d3d512335d6a3f3d8e33b04556
Larissa Duarte Santana <52838539+larissantana27@users.noreply.github.com>
2024-03-16 13:04:17 -0300

    @Z4xSuWyz - Adding getHealthInfoBooleans on Health Center Controller

--- src/main/java/com/example/NaquelesDias/service/AccountService.java
+++ src/main/java/com/example/NaquelesDias/service/AccountService.java
@@ -1,16 +1,21 @@
 package com.example.NaquelesDias.service;
 
+import com.example.NaquelesDias.model.user.UserRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Date;
 
 import static org.apache.logging.log4j.util.Strings.concat;
 
@@ -22,16 +27,27 @@ public class AccountService {
     @Autowired
     private Gson gson = new Gson();
 
+    @Autowired
+    UserRepository userRepository;
+
     public String getAccountInfoList(int userId) {
         String sql = "SELECT U.firstname, U.lastname, U.email, U.levelOfEducation, " +
                 "BI.birthday, BI.biological_sex, BI.weight , BI.gender" + "AI.street, AI.number, AI.city, AI.state"+
                 "FROM user U " +
                 "INNER JOIN biological_information BI ON U.biological_info_id = BI.id " +
-                "INNER JOIN adress_information AI ON U.adress_info_id = AI.id " +
+                "INNER JOIN address_information AI ON U.address_info_id = AI.id " +
                 "WHERE U.id = " + userId;
 
         Map<String, Object> accountInfo = jdbcTemplate.queryForObject(sql, new AccountInfoRowMapper());
         String result = convertMapToJson(accountInfo);
+        return Objects.requireNonNullElse(result, "{}");
+    }
+
+    public String getHealthInfoBooleans(int userId) throws JsonProcessingException {
+        boolean isPregnant = userRepository.findStartOfCurrentPregnancy(userId) != null;
+        ObjectMapper mapper = new ObjectMapper();
+        String result = mapper.writeValueAsString(isPregnant);
+
         return Objects.requireNonNullElse(result, "{}");
     }
 
@@ -48,8 +64,6 @@ class AccountInfoRowMapper implements RowMapper<Map<String, Object>> {
 
         String firstname = concat(resultSet.getString("firstname"), " ");
         String name = concat(firstname, resultSet.getString("lastname"));
-
-
 
         accountInfo.put("name", name);
         accountInfo.put("email", resultSet.getString("email"));
